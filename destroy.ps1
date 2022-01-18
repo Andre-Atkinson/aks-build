1d94308fbcab72914bb33ffdb19cac045a52344b
anjo-Lagrimas <74160430+anjo-Lagrimas@users.noreply.github.com>
2022-01-18 15:46:33 +0800

    main

--- destroy.ps1
+++ destroy.ps1
@@ -1,3 +1,10 @@
+#get AppID details
+$secrets = Get-Content .\sp.txt | ConvertFrom-Json
+Set-Item -Path env:TF_VAR_username -Value "$env:USERNAME"
+Set-Item -Path env:TF_VAR_appId -Value $secrets.appId
+Set-Item -Path env:TF_VAR_password -Value $secrets.password
+Set-Item -Path env:TF_VAR_tenant -Value $secrets.tenant
+
 #Delete K10 
 write-host "Deleting Kasten and Pacman" -ForegroundColor Green
 Set-Location '.\3 helm'
